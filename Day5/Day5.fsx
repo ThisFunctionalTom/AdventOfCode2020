8af73a90e41545f7f7cc28bb85bfe5fc3b767a05
tom <leko.tomas@fronius.com>
2020-12-05 07:38:08 +0100

    Day5 alternative

--- Day5/Day5.fsx
+++ Day5/Day5.fsx
@@ -33,3 +33,28 @@ File.ReadAllLines(__SOURCE_DIRECTORY__ + "/Day5.input")
 |> Array.pairwise
 |> Array.find (fun (x, y) -> y - x <> 1)
 |> fun (x, y) -> y - 1
+
+// Alternative
+
+let toSeatNumber boardingPassNr =
+    boardingPassNr
+    |> String.map (function | 'F' -> '0' | 'B' -> '1' | 'R' -> '1' | 'L' -> '0' | c -> c)
+    |> sprintf "0b%s"
+    |> int
+
+let seatNumbers = 
+    File.ReadAllLines(__SOURCE_DIRECTORY__ + "/Day5.input")
+    |> Array.map toSeatNumber
+
+let solve1 seatNumbers = 
+    Array.max seatNumbers
+
+let solve2 seatNumbers =
+    Array.sort seatNumbers
+    |> Array.pairwise
+    |> Array.find (fun (p, n) -> n - p <> 1)
+    |> fun (p, n) -> n - 1
+
+solve1 seatNumbers
+solve2 seatNumbers
+
