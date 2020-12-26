3658ed389b1dc0f3fb5d6e1006d5dcf1676899c8
ThisFunctionalTom <tomas.leko@gmail.com>
2020-12-26 19:23:09 +0100

    Day21 part2

--- Day21/Day21.fsx
+++ Day21/Day21.fsx
@@ -10,29 +10,70 @@ let readInput fileName =
     |> File.ReadAllLines
     |> Array.map parseLine
 
+let getMappings (input: (Set<string>*Set<string>)[]) =
+    input
+    |> Array.collect (fun (ings, algs) ->
+        algs
+        |> Set.toArray
+        |> Array.map (fun alg -> alg, ings))
+    |> Array.groupBy fst
+    |> Array.map (fun (alg, arr) -> 
+        alg, arr |> Array.map snd |> Array.reduce Set.intersect)
+
 let solve1 fileName =
-    let example = readInput fileName
+    let input = readInput fileName
+    let mappings = getMappings input
+
     let possibleIngs =
-        example
-        |> Array.collect (fun (ings, algs) ->
-            algs
-            |> Set.toArray
-            |> Array.map (fun alg -> alg, ings))
-        |> Array.groupBy fst
-        |> Array.map (fun (alg, arr) -> 
-            alg, arr |> Array.map snd |> Array.reduce Set.intersect)
+        mappings
         |> Array.collect (snd >> Set.toArray)
         |> Set.ofArray
 
     let allIngs = 
-        example 
+        input 
         |> Array.collect (fst >> Set.toArray) 
         |> Set.ofArray
 
     let noAlgs = Set.difference allIngs possibleIngs
 
-    example
+    input
     |> Array.sumBy (fun (ings, algs) -> Set.intersect ings noAlgs |> Set.count)
 
 solve1 "example.input"
 solve1 "Day21.input"
+
+let solveMappings (input: (string*Set<string>)[]) =
+    let rec loop (algs: (string*Set<string>) []) =
+        let single =
+            algs
+            |> Array.map snd
+            |> Array.filter (Set.count >> ((=) 1))
+            |> Array.collect Set.toArray
+            |> Set.ofArray
+
+        printfn $"Found: {single.Count}/algs.Length"
+        if single.Count = algs.Length 
+        then algs |> Array.map (fun (alg, ings) -> alg, Set.toArray ings |> Array.head)
+        else 
+            algs
+            |> Array.map (fun (alg, ings) ->
+                if Set.count ings = 1 
+                then alg, ings
+                else alg, Set.difference ings single)
+            |> loop
+
+    loop input
+
+let solve2 fileName =
+    let mappings =
+        readInput fileName
+        |> getMappings
+        |> solveMappings
+    
+    mappings
+    |> Array.sortBy fst
+    |> Array.map snd
+    |> String.concat ","
+
+solve2 "example.input"
+solve2 "Day21.input"
