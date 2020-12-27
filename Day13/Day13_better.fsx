f2595ef78e3e4efd87be8fbb384c8b39c50b60b1
ThisFunctionalTom <tomas.leko@gmail.com>
2020-12-27 19:14:39 +0100

    Day13 better

--- Day13/Day13_better.fsx
+++ Day13/Day13_better.fsx
@@ -10,17 +10,22 @@ let solve (busses: (uint64*uint64)[]) =
         match busses with
         | [] -> start
         | (busOffset, bus)::rest ->
-            let start = 0UL
-            let busOffset, bus = busses.[1]
-            let step = 19UL
             let [| nextStart; nextStartAndStep |] =
                 Seq.initInfinite (fun i -> uint64 i * step + start)
-                |> Seq.filter (fun z -> (z - busOffset) % bus = 0UL)
+                |> Seq.filter (fun z -> (z + busOffset) % bus = 0UL)
                 |> Seq.take 2
                 |> Array.ofSeq
             let nextStep = nextStartAndStep - nextStart
-            loop nextStart nextStep rest
+            loop (nextStart, nextStep) rest
 
-    loop 0UL (busses.[0] busses
+    loop (busses.[0]) (busses |> List.ofArray)
+    |> sprintf "%d"
 
-let busses = parse "19,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,523,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,17,13,x,x,x,x,x,x,x,x,x,x,29,x,853,x,x,x,x,x,37,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,23"
+solve (parse "17,x,13,19") // 3417
+solve (parse "67,7,59,61") // 754018
+solve (parse "67,x,7,59,61") // 779210 
+solve (parse "67,7,x,59,61") // 1261476 
+solve (parse "1789,37,47,1889") // 1202161486 
+
+parse "19,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,523,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,17,13,x,x,x,x,x,x,x,x,x,x,29,x,853,x,x,x,x,x,37,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,23"
+|> solve // 210612924879242
